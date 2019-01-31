d54a5694fcda8d82b2ea256f42a19acb170f68a8
Isaque Galdino <igaldino@gmail.com>
2019-01-31 11:09:25 -0200

    wm-lock: Fixed resolution

--- wm-lock.sh
+++ wm-lock.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 WALLPAPER=`grep file ~/.config/nitrogen/bg-saved.cfg | awk -F "=" '{print $2}'`
+RESOLUTION=`xrandr | grep \* | awk '{print $1}'`
+IMAGE=~/.cache/i3lock.png
 
-i3lock -i "${WALLPAPER}" -e -f
-sleep 10
+convert "$WALLPAPER" -resize $RESOLUTION^ -gravity center $IMAGE
+i3lock -i $IMAGE -e -f
+
+# Turn screen off
+sleep 60
 pgrep i3lock && xset dpms force off
