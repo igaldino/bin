b236edf0ac25e1c7bbd720223d1ba3fdb7273d6f
Isaque Galdino <igaldino@gmail.com>
2020-02-17 16:24:31 -0300

    git-audit: change date format to iso

--- git-audit.sh
+++ git-audit.sh
@@ -3,7 +3,7 @@
 echo FILENAME,CREATED BY,CREATION DATE,MODIFIED BY,MODIFIED DATE
 for i in `git ls-files`
 do
-	LASTCOMMIT=`git log -1 --pretty=format:"%ae,%ad" -- $i`
-	FIRSTCOMMIT=`git log -1 --pretty=format:"%ae,%ad" --diff-filter=A -- $i`
+	LASTCOMMIT=`git log -1 --pretty=format:"%ae,%ad" --date=iso -- $i`
+	FIRSTCOMMIT=`git log -1 --pretty=format:"%ae,%ad" --date=iso --diff-filter=A -- $i`
 	echo $i,$FIRSTCOMMIT,$LASTCOMMIT
 done
