5c5d29a42c1c8ba18c065b93048527b1270f89d4
Isaque Galdino <isaque@br.ibm.com>
2023-08-01 21:15:33 -0300

    box: fix vscode exclusion

--- box.sh
+++ box.sh
@@ -5,9 +5,12 @@ MYUSER=`whoami`
 
 rclone sync --stats-one-line -v --skip-links \
   ${HOME} box:bkp/${HOST} \
+  --exclude=**/.angular/** \
   --exclude=bkp/** \
   --exclude=**/builddir/** \
   --exclude=.cache/** \
+  --exclude=.certponto/** \
+  --exclude=.cisco/** \
   --exclude=.config/** \
   --exclude=eclipse/** \
   --exclude=**/.git/** \
@@ -19,6 +22,7 @@ rclone sync --stats-one-line -v --skip-links \
   --exclude=.npm/** \
   --exclude=.steam/** \
   --exclude=.var/** \
+  --exclude=.vscode/** \
   --exclude=**/WebSphere/** \
   --exclude=.wine/** \
 
